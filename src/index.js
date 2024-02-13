e39be6f8488288eab4188b92ab9006b02851b33a
tessaflower <tessaleighflower@gmail.com>
2024-02-12 20:18:02 -0500

    injected forecast html from javascript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">⛅️</div>
+      <div class="weather-forecast-temperature"> 
+        <strong>18°</strong>
+      </div>
+      <div class="weather-forecast-temperature">12°</div>
+    </div>
+  </div>
+  `;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Washington");
+displayForecast();
